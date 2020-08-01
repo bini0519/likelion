b4c28bb4ca8342c489dcdd88f63cfddaf6e0b639
bini0519 <qlstn9150@gmail.com>
2020-08-01 17:31:31 +0900

    오타수정

--- djangoproject/wordcount/models.py
+++ djangoproject/wordcount/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
-# Create your models here.
 class Designer(models.Model):
-    image = models.ImageField(upload_to = 'imanges/')
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=255)
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length = 50)
+    address = models.CharField(max_length = 255)
     description = models.TextField()
 
     def __str__(self):
         return self.name
+
+    def summary(self):
+        if len(self.description) >= 40:
+            return self.description[:40] + str(" ...")
+        else:
+            return self.description
