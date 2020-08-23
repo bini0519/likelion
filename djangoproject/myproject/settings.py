4e42ef9f63af31b9715ec8067dd0a1934e665bb3
bini0519 <qlstn9150@gmail.com>
2020-08-23 15:34:55 +0900

    키 설정

--- djangoproject/myproject/settings.py
+++ djangoproject/myproject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2d1mc+nb5jesm!!6i$cs-iipbkg5561u)p5b^x38&40osgcx!c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
